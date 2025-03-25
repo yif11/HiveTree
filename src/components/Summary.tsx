479262449c4e91122bfc76f61822e682e26863cb
yif11 <kinohhanten@gmail.com>
2025-03-25 10:12:36 +0000

    update: コメントを一時的にサマリ表示エリアに表示

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -3,16 +3,19 @@ import { useState } from "react";
 
 export const Summary: React.FC = () => {
 	const [comment, setComment] = useState("");
+	const [summary, setSummary] = useState("");
 
-	// テキストエリアに変化が起こったとき，その中の値を`comment`変数にセットする
+	// テキストエリアに変化が起こったとき，その中の値を`comment`変数にセット
 	const handleCommentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
 		setComment(e.target.value);
 	};
 
-	// Submit Comment ボタンが押されたとき，アラートを表示し，`comment`変数をリセットする
+	// Submit Comment ボタンが押されたとき，アラートを表示し，`comment`変数をリセット
+	// また，`summary`変数に`comment`変数の値をセット
 	const handleCommentSubmit = () => {
-		alert(`Submitted comment: ${comment}`);
+		// alert(`Submitted comment: ${comment}`);
 		setComment("");
+		setSummary(comment);
 	};
 
 	return (
@@ -22,6 +25,7 @@ export const Summary: React.FC = () => {
 			{/* トピック */}
 			<div className="topic bg-gray-400 p-2 mb-2">
 				<h2 className="text-2xl">topic</h2>
+				example topic
 			</div>
 
 			{/* コメント */}
@@ -45,6 +49,7 @@ export const Summary: React.FC = () => {
 			{/* サマリ */}
 			<div className="summary text-2xl bg-red-400 p-2 mb-2">
 				<h2 className="text-2xl">summary</h2>
+				<span>(Submitted comment: {summary})</span>
 			</div>
 		</div>
 	);
