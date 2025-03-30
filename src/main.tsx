213b161077f9e7cbe06938a443b7b534b03525aa
cube-zz <znynpx617@gmail.com>
2025-03-30 23:36:05 +0900

    他のファイルをupする

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Summary } from "./components/Summary.tsx";
+import MapPostArea from "./components/MapPostArea.tsx";
+
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<Summary />
+		<MapPostArea />
 	</StrictMode>,
 );
