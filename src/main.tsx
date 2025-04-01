9e4be1f6111e36f91be06788f96d8f92edf3ef28
yif11 <kinohhanten@gmail.com>
2025-04-01 11:47:50 +0000

    add: TopicList

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { Summary } from "./components/Summary.tsx";
+import { TopicList } from "./pages/TopicList.tsx";
+// import { Summary } from "./pages/Summary.tsx";
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<Summary />
+		{/* <Summary /> */}
+		<TopicList />
 	</StrictMode>,
 );
---
+++ src/pages/TopicList.tsx
@@ -0,0 +1,52 @@
+import type React from "react";
+import { useState } from "react";
+import useSWR from "swr";
+import { getTopic } from "../api/topic";
+
+export const TopicList: React.FC = () => {
+	const [topicUrl, setTopicUrl] = useState("");
+	const [topicTitle, setTopicTitle] = useState("");
+	const [topicSummary, setTopicSummary] = useState("");
+
+	const { error: topicError } = useSWR(
+		"/topic",
+		async () => {
+			const topics = await getTopic();
+			setTopicUrl(topics.length > 0 ? topics[0].url : "No URL");
+			setTopicTitle(topics.length > 0 ? topics[0].title : "No Title");
+			setTopicSummary(topics.length > 0 ? topics[0].summary : "No Summary");
+		},
+		{
+			refreshInterval: 3600000, // 3600秒ごとにポーリング
+		},
+	);
+
+	return (
+		// メインコンテナ
+		<div className="summary-container h-screen w-screen overflow-auto p-8 bg-gradient-to-br from-gray-100 to-gray-200 font-sans">
+			<h1 className="text-4xl font-extrabold text-gray-800 tracking-tight border-b pb-4 border-gray-300">
+				🌟 Summary of Comments on Topic
+			</h1>
+
+			{/* トピック表示 */}
+			<div className="topic bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition duration-300">
+				<h2 className="text-2xl font-semibold text-gray-700 mb-2">📌 Topic</h2>
+				{topicError ? (
+					<p className="text-red-600">⚠️ トピックの取得に失敗しました。</p>
+				) : (
+					<>
+						<p className="text-red-700 text-lg leading-relaxed">
+							{topicUrl || "（トピックURL取得中）"}
+						</p>
+						<p className="text-red-700 text-lg leading-relaxed">
+							{topicTitle || "（トピックタイトル取得中）"}
+						</p>
+						<p className="text-red-700 text-lg leading-relaxed">
+							{topicSummary || "（トピックサマリ取得中）"}
+						</p>
+					</>
+				)}
+			</div>
+		</div>
+	);
+};
