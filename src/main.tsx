2908c8575165952af687c4036f9c3e595c4fc24e
cube-zz <znynpx617@gmail.com>
2025-04-02 13:56:19 +0900

    WIP: local changes to main.tsx before merge

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { Summary } from "./components/Summary.tsx";
 import MapPostArea from "./components/MapPostArea.tsx";
-
+import { Summary } from "./components/Summary.tsx";
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
