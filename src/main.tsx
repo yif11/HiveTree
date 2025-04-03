8ef19098143474cef89086e59cf2d47f9bf1db88
cube-zz <znynpx617@gmail.com>
2025-04-03 21:15:06 +0900

    MapPostAreaを消して元に戻す

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import MapPostArea from "./pages/MapPostArea.tsx";
 import App from "./App.tsx";
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<App />
-		<MapPostArea />
 	</StrictMode>,
-);//MapPostAreaをテスト用に追加している。
+);
